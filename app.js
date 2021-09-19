36cd32dc8445589a67aaf88e5844125afa00692a
zach <ZCroninDev@users.noreply.github.com>
2021-09-19 17:21:05 -0400

    lose case and game over, rewrite of game over

--- app.js
+++ app.js
@@ -36,17 +36,48 @@ guessBtn.addEventListener('click', function(){
 
   // Check if won
   if(guess === winningNum){
-    // Disable input
-    guessInput.disabled = true;
-    // Change border color
-    guessInput.style.borderColor = 'green';
-    // Set message color
-    setMessage(`${winningNum} is correct, YOU WIN!` , 'green');
+    // Game over - won
+
+   gameOver(true, `${winningNum} is correct, YOU WIN!`)
 
   } else {
+    // Wrong number
+    guessesLeft -= 1;
 
+    if(guessesLeft === 0){
+      // Game over - lost
+
+      gameOver(false, `Game Over! Sorry you lost. The correct number was ${winningNum}`);
+
+    } else {
+      // Game continues - answer wrong
+
+      // Change border color
+      guessInput.style.borderColor = 'red';
+
+      // Clear input
+      guessInput.value = '';
+
+      // Tell user it was the wrong number
+      setMessage(`${guess} is not correct, you have ${guessesLeft} guesses left`)
+    }
   }
 });
+
+// Game over
+function gameOver(won, msg){
+  let color;
+  won === true ? color = 'green' : color = 'red';
+
+  guessInput.disabled = true;
+  // Change border color
+  guessInput.style.borderColor = color;
+  // Set message color
+  message.style.color = color;
+  // Set message
+  setMessage(msg);
+
+}
 
 // Set message
 function setMessage(msg, color){
